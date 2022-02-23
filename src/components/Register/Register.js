71948e7d9c73e483e0a20880e6e9192c5dc4375e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-02-24 01:14:05 +0600

    form created

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const [info,setInfo]=useState([]);
     const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+      setInfo(data);
+    }
 
   return (
+    <>
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} />
-      <select {...register("gender")}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
+    
+      <input {...register("firstName", { required: true, maxLength: 20 })} />
+      <br/>
+      <br/>
+      <input {...register("lastName")} />
+      <br/>
+      <br/>
+
+      <input {...register("email")} />
+      <br/>
+      <br/>
+      
+
       <input type="submit" />
+ 
     </form>
+
+    <br/>
+    <br/>
+    <h2>{info.firstName}</h2>
+    <h3>{info.lastName}</h3>
+    <p>{info.email}</p>
+    </>
+
+   
   );
 }
        
